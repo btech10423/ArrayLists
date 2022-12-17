6efdc4814bd2e0674c5b9d717aeb1f0ed5e7c761
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-17 17:09:52 +0530

    Add files via upload

---
+++ PrintreverseAL.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class PrintreverseAL{
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        for(int i=list.size()-1;i>=0;i--){
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+        }
+    }
