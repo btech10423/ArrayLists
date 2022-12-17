5eef9f20e9ed7c94838509993545823fdd81ae3e
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-17 17:09:28 +0530

    Add files via upload

---
+++ SizeofAL.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class SizeofAL{
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list.size());
+
+        for(int i=0;i<list.size(); i++){
+            System.out.print(list.get(i)+" ");
+        }
+        System.out.println();
+    }
+}
