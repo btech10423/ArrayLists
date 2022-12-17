71306b532627702f48511d1da9055935c9d0532b
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-17 12:43:09 +0530

    Add files via upload

---
+++ Arraylists.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class Arraylists{
+    public static void main(String args[]){
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+    }
+}
