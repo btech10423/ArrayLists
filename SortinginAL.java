955b77a17a77ef1f0dd4cd5dfb8f8fb0a1027ce7
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-12-17 17:11:41 +0530

    Add files via upload

---
+++ SortinginAL.java
@@ -0,0 +1,22 @@
+//import java.util.ArrayList;
+//import java.util.Collections;
+import java.util.*;
+public class SortinginAL{
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        list.add(2);
+        list.add(5);
+        list.add(9);
+        list.add(3);
+        list.add(6);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println(list);
+    }
+}
+
