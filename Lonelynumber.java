a6953c8d3eede2e9c772578ee3d04243a14651a4
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2023-01-19 14:04:39 +0530

    Add files via upload

---
+++ Lonelynumber.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Lonelynumber {
+    public static ArrayList<Integer> findlonely(ArrayList<Integer> nums) {
+        Collections.sort(nums);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i < nums.size() - 1; i++) {
+            if (nums.get(i - 1) + 1 < nums.get(i) && nums.get(i) + 1 < nums.get(i + 1)) {
+                list.add(nums.get(i));
+            }
+        }
+        if (nums.size() == 1) {
+            list.add(nums.get(0));
+        }
+        if (nums.size() > 1) {
+            if (nums.get(0) + 1 < nums.get(1)) {
+                list.add(nums.get(0));
+            }
+            if (nums.get(nums.size() - 2) + 1 < nums.get(nums.size() - 1)) {
+                list.add(nums.get(nums.size() - 1));
+            }
+        }
+        return list;
+    }
+
+    public static void main(String args[]) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(3);
+        nums.add(5);
+        nums.add(3);
+        System.out.println(findlonely(nums));
+    }
+}
